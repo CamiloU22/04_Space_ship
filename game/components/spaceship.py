2ea1108c0241c792c62004f7aa4f470592ed7886
CamiloU22 <136392095+CamiloU22@users.noreply.github.com>
2023-06-13 08:17:51 -0500

    Actualisacion clase uno

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -16,10 +16,14 @@ class Spaceship(Sprite):
     def move_left(self):
         if self.rect.left > 0:
             self.rect.x = self.rect.x - 10
+        else :
+            self.rect.x = 1100
 
     def move_right(self):
         if self.rect.right < SCREEN_WIDTH:
             self.rect.x =self.rect.x + 10
+        else:
+            self.rect.x = 0
 
     def move_up(self):
         if self.rect.y > SCREEN_HEIGHT // 2:
